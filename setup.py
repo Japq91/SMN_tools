71bb55af946d7d2749e204dea1b40869183c8869
Japq91 <20090143@lamolina.edu.pe>
2025-09-16 19:16:06 -0300

    Actualizar configuración del proyecto y documentación

--- setup.py
+++ setup.py
@@ -11,13 +11,19 @@ setup(
     packages=find_packages("src"),
     package_dir={"": "src"},
     install_requires=[
-        "xarray",
-        "numpy",
-        "netCDF4",
-        "cfgrib",
-        "h5netcdf",
-        "ipykernel",   # <-- aquí
+        "xarray>=2023.7.0",
+        "cfgrib>=0.9.10.4",
+        "numpy>=1.24.3",
+        "netCDF4>=1.6.2",
+        "h5netcdf>=1.2.0",
+        "pandas>=2.0.3",
+        "eccodes>=2.27.0",
+        "h5py>=3.8.0",
+        "ipykernel>=6.0.0",
+        "matplotlib>=3.7.0",
+        "pillow>=9.0.0",
     ],
+
 )
 entry_points={
     "console_scripts": [
--- src/SMN_tools/delete_files.py
+++ src/SMN_tools/delete_files.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 import os
 
 def clean_outdir(outdir: str):
