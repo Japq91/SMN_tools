edb3d6ef1fe64dfdd23e106e4d8ffef8d4c09067
Japq91 <20090143@lamolina.edu.pe>
2025-09-30 08:46:28 -0300

    Readme.md update2

--- cods/prueba/busca_radiacion.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-gribfile='/scratch/Datatemporal/SMN/data/regional/PERU_WRF22/202501/2025010106/WRFPRS_d01.06'
-grib_ls -w shortName=ssrd $gridfile
-grib_ls -w shortName=dswrf $gribfile
-grib_ls -p shortName,name,paramId,typeOfLevel,level,stepType $gribfile | grep -Ei 'ssrd|dswrf|swrad|short|solar'
-
